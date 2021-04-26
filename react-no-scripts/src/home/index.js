f2a57e962de6c06beead936b90a9844a88009ba9
qudawei <qudawei@qudaweideMacBook-Pro.local>
2021-04-26 19:53:12 +0800

    test

---
+++ react-no-scripts/src/home/index.js
@@ -0,0 +1,21 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Author: David Qu
+ * @Date: 2021-04-26 19:25:53
+ * @LastEditTime: 2021-04-26 19:48:44
+ * @LastEditors: David Qu
+ */
+import React from 'react'
+import "./test.css"
+// import awardImg from "../../assets/img/awardImg.png"
+export default class Home extends React.Component {
+    render(){
+        return (
+            <div className="test test2">
+                <p>hello world123</p>
+                {/* <img src={awardImg} alt="" style={{width:360,height:60}}/> */}
+            </div>
+        )
+    }
+}
