fc01fc5ee5360aa04b7f9d4772ebb5dedd297e2f
qudawei <qudawei@58.com>
2021-07-21 10:35:10 +0800

    啦啦啦

---
+++ js/test1.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Version: 
+ * @Author: David Qu
+ * @Date: 2021-07-20 15:57:29
+ * @LastEditors: David Qu
+ * @LastEditTime: 2021-07-20 18:05:15
+ */
+// 1111
+// 1111
+// 1111
+// 1111
+// 1111
+// 1111
+    console.log('1111', 'script1');
+    alert(1111)
