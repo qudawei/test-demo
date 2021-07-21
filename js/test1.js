b89ea307d54c4a261b1c93c7e62a921601dc6abd
qudawei <qudawei@58.com>
2021-07-21 11:22:04 +0800

    Revert "第三次提交"
    This reverts commit 3d2561eed5acfb13f6d9048cb8854071a9cba43f.

--- js/test1.js
+++ js/test1.js
@@ -4,7 +4,7 @@
  * @Author: David Qu
  * @Date: 2021-07-20 15:57:29
  * @LastEditors: David Qu
- * @LastEditTime: 2021-07-21 11:17:45
+ * @LastEditTime: 2021-07-21 11:17:08
  */
 // 1111
 // 1111
@@ -14,5 +14,3 @@
 // 1111
 // 第二次提交
     console.log(2222);
-// 第三次提交
-console.log(3333);
