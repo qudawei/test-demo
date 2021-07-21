6b6de39382fb9f409c09cd2876fbd539ae56568d
qudawei <qudawei@58.com>
2021-07-21 10:46:25 +0800

    测试revert

--- js/test1.js
+++ js/test1.js
@@ -4,7 +4,7 @@
  * @Author: David Qu
  * @Date: 2021-07-20 15:57:29
  * @LastEditors: David Qu
- * @LastEditTime: 2021-07-20 18:05:15
+ * @LastEditTime: 2021-07-21 10:46:12
  */
 // 1111
 // 1111
@@ -14,3 +14,4 @@
 // 1111
     console.log('1111', 'script1');
     alert(1111)
+    console.log('llllalsldlfslf')
